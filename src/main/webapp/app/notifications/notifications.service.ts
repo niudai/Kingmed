8c304ead57f9247ec41260f04498508c0b1d9091
DaiNiu <1244453736@qq.com>
2019-08-28 23:11:17 +0800

    generated ntf rest service

---
+++ src/main/webapp/app/notifications/notifications.service.ts
@@ -0,0 +1,29 @@
+import { HttpResponse, HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SERVER_API_URL } from 'app/app.constants';
+import { Observable } from 'rxjs';
+
+type NotificationResponse = HttpResponse<INotification>;
+type NotificationArrayResponse = HttpResponse<INotification[]>;
+@Injectable()
+export class NotificationService {
+resourceUrl = SERVER_API_URL +  'api/notifications';
+constructor(private http: HttpClient) { }
+create(label: INotification): Observable<NotificationResponse> {
+return this.http.post<INotification>(this.resourceUrl, label, { observe: 'response' });
+}
+update(label: INotification): Observable<NotificationResponse> {
+return this.http.put<INotification>(this.resourceUrl, label, { observe: 'response' });
+}
+find(id: number): Observable<NotificationResponse> {
+return this.http.get<INotification>(`${this.resourceUrl}/id`, { observe: 'response' });
+}
+query(req?: any): Observable<NotificationArrayResponse> {
+const options = createRequestOption(req);
+return this.http.get<INotification[]>(this.resourceUrl, { params: options, observe: 'response' });
+}
+delete(id: number): Observable<NotificationResponse> {
+return this.http.delete<any>(`${this.resourceUrl}/id`, { observe: 'response' });
+}
+}
+}
