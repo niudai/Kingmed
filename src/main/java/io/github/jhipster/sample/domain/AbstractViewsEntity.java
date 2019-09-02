e3025d91fea65a902ab0fa4ad6d719983af70424
DaiNiu <1244453736@qq.com>
2019-09-02 21:16:24 +0800

    fixed bug about back to top func in fab #2 hide the back to top btn on current pageXoffset

--- src/main/java/io/github/jhipster/sample/domain/AbstractViewsEntity.java
+++ src/main/java/io/github/jhipster/sample/domain/AbstractViewsEntity.java
@@ -28,22 +28,18 @@ public class AbstractViewsEntity implements Serializable {
 
     @CreatedBy
     @Column(name = "created_by", nullable = false, length = 50, updatable = false)
-    @JsonIgnore
     private String createdBy;
 
     @CreatedDate
     @Column(name = "created_date", updatable = false)
-    @JsonIgnore
     private Instant createdDate = Instant.now();
 
     @LastModifiedBy
     @Column(name = "last_modified_by", length = 50)
-    @JsonIgnore
     private String lastModifiedBy;
 
     @LastModifiedDate
     @Column(name = "last_modified_date")
-    @JsonIgnore
     private Instant lastModifiedDate = Instant.now();
 
     public String getCreatedBy() {
--- src/main/webapp/app/layouts/navbar/floating-action-btn/floating-action-btn.component.ts
+++ src/main/webapp/app/layouts/navbar/floating-action-btn/floating-action-btn.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { MatDialog, MatSnackBar } from '@angular/material';
 import { faWeixin } from '@fortawesome/free-brands-svg-icons';
 import { IFeedback } from 'app/shared/model/feedback.model';
@@ -6,46 +6,56 @@ import { FeedbackDialogComponent } from '../feedback-dialog/feedback-dialog.comp
 import { FeedbackService } from '../feedback-dialog/feedback.service';
 
 @Component({
-  selector: 'jhi-floating-action-btn',
-  templateUrl: './floating-action-btn.component.html',
-  styleUrls: ['./floating-action-btn.component.css']
+    selector: 'jhi-floating-action-btn',
+    templateUrl: './floating-action-btn.component.html',
+    styleUrls: ['./floating-action-btn.component.css']
 })
 export class FloatingActionBtnComponent implements OnInit {
-  faWeixin = faWeixin;
-  QRCODE_WIDTH = 100;
-  widthOfQRCode: number;
-  feedback: IFeedback;
-  feedbackSuccessMsg: string = '反馈成功';
-  constructor(
-    private dialog: MatDialog,
-    private service: FeedbackService,
-    private snackBar: MatSnackBar) { }
+    faWeixin = faWeixin;
+    QRCODE_WIDTH = 100;
+    widthOfQRCode: number;
+    feedback: IFeedback;
+    feedbackSuccessMsg = '反馈成功';
+    pageXOffset: number;
+    topBtnHeight = 'startHeight';
+    constructor(private dialog: MatDialog, private service: FeedbackService, private snackBar: MatSnackBar) {}
 
-  ngOnInit() {
-    this.widthOfQRCode = 0;
-  }
+    ngOnInit() {
+        this.widthOfQRCode = 0;
+    }
 
-  openDialog(): void {
-    const dialogRef = this.dialog.open(FeedbackDialogComponent, {
-      width: '600px',
-      data: { content: '', phone: '' }
-    });
+    @HostListener('window:scroll', ['$event'])
+    onScroll() {
+        if (window.pageYOffset < window.innerHeight) {
+            this.topBtnHeight = '0';
+        } else {
+            this.topBtnHeight = '60px';
+        }
+        console.log(this.topBtnHeight);
+    }
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      this.feedback = result;
-      this.service.create(this.feedback).subscribe(
-        any => this.snackBar.open(this.feedbackSuccessMsg)
-      )
-    });
-  }
+    openDialog(): void {
+        const dialogRef = this.dialog.open(FeedbackDialogComponent, {
+            width: '600px',
+            data: { content: '', phone: '' }
+        });
 
-  showWechatQRcode() {
-    this.widthOfQRCode = this.QRCODE_WIDTH;
-  }
+        dialogRef.afterClosed().subscribe(result => {
+            console.log('The dialog was closed');
+            this.feedback = result;
+            this.service.create(this.feedback).subscribe(any => this.snackBar.open(this.feedbackSuccessMsg));
+        });
+    }
 
-  hideWechatQRcode() {
-    this.widthOfQRCode = 0;
-  }
+    scrollToTop() {
+        window.scrollTo(pageXOffset, 0);
+    }
 
+    showWechatQRcode() {
+        this.widthOfQRCode = this.QRCODE_WIDTH;
+    }
+
+    hideWechatQRcode() {
+        this.widthOfQRCode = 0;
+    }
 }
